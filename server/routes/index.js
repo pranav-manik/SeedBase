aff820551f83ca9f1bf5fd2a1c8f32fd333c40c1
Pranav <pranav.manik@gmail.com>
2019-08-14 23:15:20 -0400

    fuzzy searching (multiphrasing)

--- server/routes/index.js
+++ server/routes/index.js
@@ -12,18 +12,141 @@ router.get('/', function(req, res, next) {
 //grabs search
 router.get('/search', function(req,res,next) {
 	var search = req.query.search;
+	var search_array = search.split(" ");
 	var result_size = 200;
-	var query = {
-        "dis_max" : {
-            "queries" : [
-                { "fuzzy" : { "variety" : search }},
-                { "fuzzy" : { "name" : search }},
-                { "fuzzy" : { "category" : search }},
+	//if searching 1 word
+	if (search_array.length == 1)
+	{
+		var query = {
+	        "dis_max" : {
+	            "queries" : [
+	                { 
+	                	"fuzzy" : { 
+	                		"variety" : {
+			                    "value": search,
+			                    "boost": 1.0,
+								"fuzziness": 2,
+								"prefix_length": 2,
+								"max_expansions": 20
+			                 }
+	            		}
+	            	},
+	                { 
+	                	"fuzzy" : { 
+	                		"name" : {
+			                    "value": search,
+			                    "boost": 1.0,
+								"fuzziness": 2,
+								"prefix_length": 2,
+								"max_expansions": 20
+			                 }
+	            		}
+	            	},
+	                { 
+	                	"fuzzy" : { 
+	                		"category" : {
+			                    "value": search,
+			                    "boost": 1.0,
+								"fuzziness": 2,
+								"prefix_length": 2,
+								"max_expansions": 20
+			                 }
+	            		}
+	            	}
 
-            ],
-            "tie_breaker" : 0.7
-        }
-    }
+	            ],
+	            "tie_breaker" : 0.7
+	        }
+	    }
+	}
+
+
+	//if search query is more than 1 word
+	else if (search_array.length > 1)
+	{
+		//clause for variety
+		var search_variety_clause = [];
+		search_array.forEach( (word) => {
+			clause = {
+	          "span_multi": {
+	            "match": {
+	              "fuzzy": {
+	                "variety": {
+	                  "fuzziness": "2",
+	                  "value": word
+	                }
+	              }
+	            }
+	          }
+	        }
+			search_variety_clause.push(clause);
+		});
+		// clause for name
+		var search_name_clause = [];
+		search_array.forEach( (word) => {
+			clause = {
+	          "span_multi": {
+	            "match": {
+	              "fuzzy": {
+	                "name": {
+	                  "fuzziness": "2",
+	                  "value": word
+	                }
+	              }
+	            }
+	          }
+	        }
+			search_name_clause.push(clause);
+		});
+		// clause for category
+		var search_category_clause = [];
+		search_array.forEach( (word) => {
+			clause = {
+	          "span_multi": {
+	            "match": {
+	              "fuzzy": {
+	                "category": {
+	                  "fuzziness": "2",
+	                  "value": word
+	                }
+	              }
+	            }
+	          }
+	        }
+			search_category_clause.push(clause);
+		});
+
+		var query = {
+		    "dis_max" : {
+				"queries" : [
+					{
+						"span_near": {
+							"clauses": search_variety_clause,
+							"slop": 1,
+							"in_order": "true"
+				    	}
+				  	},
+					{
+				    	"span_near": {
+					      	"clauses": search_name_clause,
+					      	"slop": 1,
+					      	"in_order": "true"
+					    }
+					  },
+					{
+				    	"span_near": {
+					      	"clauses": search_category_clause,
+					      	"slop": 1,
+					      	"in_order": "true"
+				    	}
+				  	}
+				],
+				"tie_breaker" : 0.7
+		    }
+		  }
+
+	}
+
 	//Mongoose search
 	// const regex = new RegExp(escapeRegex(req.query.search), 'gi');
 	// Seed.find({ name : regex}, (err, docs) => {
